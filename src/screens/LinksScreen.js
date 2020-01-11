061d0e77f0301caf6bedbd3d2834fcb1b0c958bf
DESKTOP-130CJNK\seren <serena.li@stud.fh-campuswien.ac.at>
2020-01-11 20:24:50 +0100

    added barcode, changes in linksscreen and logger

--- src/screens/LinksScreen.js
+++ src/screens/LinksScreen.js
@@ -1,7 +1,5 @@
 import React from 'react';
-//import { ExpoLinksView } from '@expo/samples';
-//import * as Permissions from 'expo-permissions';
-//import { BarCodeScanner } from 'expo-barcode-scanner';
+
 import { CellVariant, Cell, Section, TableView } from 'react-native-tableview-simple';
 import {
   ActivityIndicator,
@@ -16,75 +14,98 @@ import {
   Alert,
   Button
 } from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import { Asset } from 'expo-asset';
-import * as SQLite from 'expo-sqlite';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
 
+export default function Logger() {
 
-///////////////////
-/*FileSystem.downloadAsync(
-  Asset.fromModule(require('../assets/db/treevydb/')),
-  `${FileSystem.documentDirectory}SQLite/treevy.db/`
-);
-*/
-const db = SQLite.openDatabase(`${FileSystem.documentDirectory}SQLite/treevy.db`);
-//////////////////
+  let state = {
+    table: [
+      {
+        qrCode: 'Tree1Log1',
+        gps: { lat: 47.21894418, long: 24.71372435 },
+        height: 100,
+        diameter: 0.8,
+        treeNumber: 1,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+      {
+        qrCode: 'Tree1Log2',
+        gps: { lat: 47.21894418, long: 24.71372435 },
+        height: 100,
+        diameter: 0.8,
+        treeNumber: 1,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+      {
+        qrCode: 'Tree1Log3',
+        gps: { lat: 47.21894418, long: 24.71372435 },
+        height: 100,
+        diameter: 0.8,
+        treeNumber: 1,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      }, 
+      {
+        qrCode: 'Tree2Log1',
+        gps: { lat: 47.71732435, long: 24.12372435 },
+        height: 30,
+        diameter: 0.6,
+        treeNumber: 2,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      }, 
+      {
+        qrCode: 'Tree3Log1',
+        gps: { lat: 47.94672435, long: 24.71497435 },
+        height: 80,
+        diameter: 0.9,
+        treeNumber: 3,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+      {
+        qrCode: 'Tree3Log2',
+        gps: { lat: 47.94672435, long: 24.71497435 },
+        height: 80,
+        diameter: 0.9,
+        treeNumber: 3,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      }, 
+      {
+        qrCode: 'Tree4Log1',
+        gps: { lat: 47.36572435, long: 24.9872435 },
+        height: 20,
+        diameter: 0.7,
+        treeNumber: 4,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+    ]
+  };
 
-export default function LinksScreen() {
-
-    let state = {
-         table: [
-             {
-                 qrCode: '002142',
-                 gps: {lat: 47.21894418, long:24.71372435},
-                 treeNumber: 1,
-                 color: "blue",
-                 idWorker: 1,
-                 date: "29/11/2019 - 12:21:00"
-             },{
-                 qrCode: '002143',
-                 gps: {lat: 47.71732435, long:24.12372435},
-                 treeNumber: 2,
-                 color: "red",
-                 idWorker: 1,
-                 date: "29/11/2019 - 12:21:00"
-             },{
-                 qrCode: '002144',
-                 gps: {lat: 47.94672435, long:24.71497435},
-                 treeNumber: 3,
-                 color: "green",
-                 idWorker: 1,
-                 date: "29/11/2019 - 12:21:00"
-             },{
-                 qrCode: '002145',
-                 gps: {lat: 47.36572435, long:24.9872435},
-                 treeNumber: 4,
-                 color: "pink",
-                 idWorker: 1,
-                 date: "29/11/2019 - 12:21:00"
-             },
-         ]
-     };
-
-     showAlert = (data) => {
-Alert.alert(
-  'Tree',
-  " Latitude: "+data.gps.lat + " \n Longitude: " + data.gps.long + " \n Number: " +data.treeNumber +
-                                     " \n Color: " + data.color + " \n Worker: "+data.idWorker + " \n Date: "+data.date,
-  [
-    {
-      text: 'Cancel',
-      onPress: () => console.log('Cancel Pressed'),
-      style: 'cancel',
-    },
-    { text: 'OK', onPress: () => console.log('OK Pressed') },
-  ],
-  { cancelable: false }
-);
-     }
+  showAlert = (data) => {
+    Alert.alert(
+      `${data.qrCode}`,
+      " Latitude: " + data.gps.lat + " \n Longitude: " + data.gps.long + " \n TreeNumber: " + data.treeNumber +
+      " \n Date: " + data.date,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false }
+    );
+  }
 
   buttonPressed = () => {
-console.log("press")
+    console.log("press")
     fetch('https://signer.testnet-0bsnetwork.com/data', {
       method: 'POST',
       headers: {
@@ -93,35 +114,37 @@ console.log("press")
       },
       body: JSON.stringify({
         seed: 'faculty heart inform tent subject stable sword crop wine cushion ugly rice wheat firm chapter',
-        data: [{"key": "json", "value": JSON.stringify(state.table) }],
+        data: [{ "key": "json", "value": JSON.stringify(state.table) }],
       }),
     })
-    .then(dataReponse  => dataReponse.json())
-    .then(dataReponse  => console.log(dataReponse));
+      .then(dataReponse => dataReponse.json())
+      .then(dataReponse => console.log(dataReponse));
+    
+    alert("Data got sent to Blockchain")
   }
 
-
   return (
-    <ScrollView style={styles.container} >
-<TableView>
-  <Section>
-  {
-                   state.table.map((data, index) =>
-                                   <Cell key={index} title={data.qrCode} style={{flex: 1, fontSize: 12}}
-                                                                       onPress={() => this.showAlert(data)}/>
-                           )
-                   }
-  </Section>
-</TableView>
-      <Button primary onPress={this.buttonPressed} title="Save Data"/>
 
+    <ScrollView style={styles.container} >
+        <Logo/>
+
+        <TableView>
+          <Section>
+            {
+              state.table.map((data, index) =>
+                <Cell key={index} title={data.qrCode} style={{ flex: 1, fontSize: 12 }}
+                  onPress={() => this.showAlert(data)} />
+              )
+            }
+          </Section>
+        </TableView>
+
+        <Button primary onPress={this.buttonPressed} title="Save Data"/>
     </ScrollView>
+
+    
   );
 }
-
-LinksScreen.navigationOptions = {
-  title: 'Links',
-};
 
 const styles = StyleSheet.create({
   container: {
--- src/screens/Logger.js
+++ src/screens/Logger.js
@@ -1,218 +1,109 @@
-import React from "react";
+import React from 'react';
+
+import { CellVariant, Cell, Section, TableView } from 'react-native-tableview-simple';
 import {
+  ActivityIndicator,
+  Dimensions,
+  Image,
   ScrollView,
   StyleSheet,
+  Switch,
   Text,
-  View,
   TextInput,
-  TouchableOpacity
-} from "react-native";
-import Constants from "expo-constants";
-//import { SQLite } from "expo-sqlite";
-import * as SQLite from 'expo-sqlite';
-import * as FileSystem from 'expo-file-system';
-import { Asset } from 'expo-asset';
+  View,
+  Alert,
+  Button
+} from 'react-native';
+import Background from '../components/Background';
+import Logo from '../components/Logo';
 
+export default function Logger() {
 
-//FileSystem.downloadAsync(
-  //Asset.fromModule(require('assets/db/tested.db')).uri, `${Expo.FileSystem.documentDirectory}SQLite/Tested.db`);
-
-const db = SQLite.openDatabase(`db.db`);
-
-class Logger extends React.Component {
-  state = {
-    items: null
-  };
-
-  componentDidMount() {
-    this.update();
-  }
-
-  render() {
-    const { done: doneHeading } = this.props;
-    const { items } = this.state;
-    const heading = doneHeading ? "Completed" : "Todo"; // alternative to if statement
-
-    if (items === null || items.length === 0) {
-      return null;
-    }
-
-    return (
-      <View style={styles.sectionContainer}>
-        <Text style={styles.sectionHeading}>{heading}</Text>
-        {items.map(({ id, done, value }) => (
-          <TouchableOpacity
-            key={id}
-            onPress={() => this.props.onPressItem && this.props.onPressItem(id)}
-            style={{
-              backgroundColor: done ? "#1c9963" : "#fff",
-              borderColor: "#000",
-              borderWidth: 1,
-              padding: 8
-            }}
-          >
-            <Text style={{ color: done ? "#fff" : "#000" }}>{value}</Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    );
-  }
-
-  update() {
-    db.transaction(tx => {
-      tx.executeSql(
-        `select * from items where done = ?;`,
-        [this.props.done ? 1 : 0],
-        (_, { rows: { _array } }) => this.setState({ items: _array })
-      );
-    });
-  }
-}
-
-
-///////////////////////////////
-
-
-export default class App extends React.Component {
-  state = {
-    text: null
-  };
-
-  componentDidMount() {
-    db.transaction(tx => {
-      tx.executeSql(
-        "create table if not exists items (id integer primary key not null, done int, value text);"
-      );
-    });
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.heading}>SQLite Example</Text>
-        <View style={styles.flexRow}>
-          <TextInput
-            onChangeText={text => this.setState({ text })}
-            onSubmitEditing={() => {
-              this.add(this.state.text);
-              this.setState({ text: null });
-            }}
-            placeholder="what do you need to do?"
-            style={styles.input}
-            value={this.state.text}
-          />
-        </View>
-        <ScrollView style={styles.listArea}>
-          <Logger
-            done={false}
-            ref={todo => (this.todo = todo)}
-            onPressItem={id =>
-              db.transaction(
-                tx => {
-                  tx.executeSql(`update items set done = 1 where id = ?;`, [
-                    id
-                  ]);
-                
-                },
-                null,
-                this.update
-              )
-            }
-          />
-          <Logger
-            done={true}
-            ref={done => (this.done = done)}
-            onPressItem={id =>
-              db.transaction(
-                tx => {
-                  tx.executeSql(`delete from items where id = ?;`, [id]);
-                },
-                null,
-                this.update
-              )
-            }
-          />
-        </ScrollView>
-      </View>
-    );
-  }
-
-  add(text) {
-    // is text empty?
-    if (text === null || text === "") {
-      return false;
-    }
-    console.log(text)
-    db.transaction(
-      tx => {
-        tx.executeSql("insert into items (done, value) values (0, ?)", [text]);
-        tx.executeSql("select * from items", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-          
-        );
+  let state = {
+    table: [
+      {
+        qrCode: 'Tree1Log1, Tree1Log2, TreeLog3',
+        gps: { lat: 47.21894418, long: 24.71372435 },
+        height: 100,
+        diameter: 0.8,
+        treeNumber: 1,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
       },
-      null,
-      this.update
+      {
+        qrCode: 'Tree2Log1',
+        gps: { lat: 47.71732435, long: 24.12372435 },
+        height: 30,
+        diameter: 0.6,
+        treeNumber: 2,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      }, 
+      {
+        qrCode: 'Tree3Log1, TreeLog2',
+        gps: { lat: 47.94672435, long: 24.71497435 },
+        height: 80,
+        diameter: 0.9,
+        treeNumber: 3,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+      {
+        qrCode: 'Tree4Log1',
+        gps: { lat: 47.36572435, long: 24.9872435 },
+        height: 20,
+        diameter: 0.7,
+        treeNumber: 4,
+        idWorker: 1,
+        date: "29/11/2019 - 12:21:00"
+      },
+    ]
+  };
+
+  showAlert = (data) => {
+    Alert.alert(
+      `${data.treeNumber}`,
+       data.qrCode ,
+      [
+        {
+          text: 'Cancel',
+          onPress: () => console.log('Cancel Pressed'),
+          style: 'cancel',
+        },
+        { text: 'OK', onPress: () => console.log('OK Pressed') },
+      ],
+      { cancelable: false }
     );
   }
 
-  update = () => {
-    this.todo && this.todo.update();
-    this.done && this.done.update();
-  };
+  return (
+
+    <ScrollView style={styles.container} >
+      <View style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between' }}>
+        <Logo/>
+
+        <TableView>
+          <Section>
+            {
+              state.table.map((data, index) =>
+                <Cell key={index} title={data.treeNumber} style={{ flex: 1, fontSize: 12 }}
+                  onPress={() => this.showAlert(data)} />
+              )
+            }
+          </Section>
+        </TableView>
+      </View>
+    </ScrollView>
+  );
 }
-
-
-///////////////////
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#fff",
     flex: 1,
-    paddingTop: Constants.statusBarHeight
+    paddingTop: 15,
+    backgroundColor: '#fff',
   },
-  heading: {
-    fontSize: 20,
-    fontWeight: "bold",
-    textAlign: "center"
-  },
-  flexRow: {
-    flexDirection: "row"
-  },
-  input: {
-    borderColor: "#4630eb",
-    borderRadius: 4,
-    borderWidth: 1,
-    flex: 1,
-    height: 48,
-    margin: 16,
-    padding: 8
-  },
-  listArea: {
-    backgroundColor: "#f0f0f0",
-    flex: 1,
-    paddingTop: 16
-  },
-  sectionContainer: {
-    marginBottom: 16,
-    marginHorizontal: 16
-  },
-  sectionHeading: {
-    fontSize: 18,
-    marginBottom: 8
-  }
 });
-
-
-Logger.navigationOptions = {
-  title: 'Logger',
-};
-
-/*    const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        paddingTop: 15,
-        backgroundColor: '#fff',
-    },
-    });
-*/
