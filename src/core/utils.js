48e8209f835d0e245531cee167c09246ceef4bd1
Judyta Krzyzak <judyta.krzyzak@stud.fh-campuswien.ac.at>
2020-01-11 19:46:54 +0100

    Login geht

--- src/core/utils.js
+++ src/core/utils.js
@@ -3,18 +3,20 @@ export const emailValidator = email => {
 
   if (!email || email.length <= 0) return 'Email cannot be empty.';
   if (!re.test(email)) return 'Ooops! We need a valid email address.';
+  if (email == "thomas.schneider@wien.at" || email == "greta.schneider@wien.at" || email == "wolfgang.beckmann@wien.at" || email == "caroline.weber@wien.at") return '';
+  else return 'Wrong email';
+
+  //if (email != "thomas.schneider@wien.at" || email != "greta.schneider@wien.at") return 'Wrong email';
+  //else if (email != "wolfgang.beckmann@wien.at") return 'Wrong email';
+  //else if (email != "greta.schneider@wien.at") return 'Wrong email';
+  //"caroline.weber@wien.at" )
 
   return '';
 };
 
 export const passwordValidator = password => {
   if (!password || password.length <= 0) return 'Password cannot be empty.';
-
-  return '';
-};
-
-export const nameValidator = name => {
-  if (!name || name.length <= 0) return 'Name cannot be empty.';
+  if (password != "treevy") return 'Wrong password';
 
   return '';
 };
